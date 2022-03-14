47ca3a031f830ad8d4087c54e7a02bdb92f5987f
cjensen506 <chris.jensen506@gmail.com>
2022-03-14 12:37:52 -0700

    webhook to slack with secret url

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+from google.cloud import firestore
+from google.cloud import secretmanager
 
+
+# Scrape the webpage
 page = requests.get('https://www.fs.usda.gov/detailfull/mthood/alerts-notices/?cid=stelprdb5191108&width=full')
 
-if(page.status_code == 200):
+if (page.status_code == 200):
     soup = BeautifulSoup(page.content, 'lxml')
-    #print(soup.prettify())
     all_tables = soup.find_all('table')
     data = []
     for table in all_tables:
@@ -13,7 +16,7 @@ if(page.status_code == 200):
         table_body = table.find('tbody')
         header_check = table_body.find("tr").find("td").text.strip()
 
-        if(header_check == 'Road Number'):  #make sure it is a table with road info in it
+        if (header_check == 'Road Number'):  # make sure it is a table with road info in it
             rows = table_body.find_all('tr')
             for row in rows:
                 cols = row.find_all('td')
@@ -21,12 +24,7 @@ if(page.status_code == 200):
                 if cols:
                     data.append([ele for ele in cols])  # Get rid of empty values
 
-
-from google.cloud import firestore
-
-# The `project` parameter is optional and represents which project the client
-# will act on behalf of. If not supplied, the client falls back to the default
-# project inferred from the environment.
+#pull from database roads of interest
 db = firestore.Client(project='fs-road-status')
 
 roads_ref = db.collection(u'roads')
@@ -39,14 +37,19 @@ for doc in docs:
         if (row[0] == road_of_interest_dict['road_number']) & (row[1] == road_of_interest_dict['road_name']):
             if (row[2] != road_of_interest_dict['status']) | (row[3] != road_of_interest_dict['conditions_updates']):
                 # change in road status
-                print("update road")
+                # update the database
                 doc.reference.update({u'status': row[2]})
                 doc.reference.update({u'conditions_updates': row[3]})
-            print("hello world")
-            print(row)
 
-# for row in data:
-#     for road in roads_of_interest:
-#         if ((row[0] == road['road_number']) & (row[1] == road['road_name'])):
-#             print("found one")
-#             print(row)
+                # send webhook alert
+                client = secretmanager.SecretManagerServiceClient()
+                name = f"projects/361740823008/secrets/webhook_url/versions/1"
+
+                # Access the secret version.
+                response = client.access_secret_version(request={"name": name})
+                webhook_url = response.payload.data.decode("UTF-8")
+
+                myobj = {'text': ' '.join([str(elem) for elem in row])}
+                x = requests.post(webhook_url, json=myobj)
+
+
